02862054883ab42e22dd69b38afbecf770715cd7
Jose Daniel Romero <danyrome12@gmail.com>
2023-06-22 21:35:38 -0500

    En proceso de conectar con la base de datos proporcionada

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,14 +1,17 @@
 import '../stylesheets/MainContent.css'
 import Stay from './Stay';
 
-function MainContent() {
+function MainContent({ stays, setStays }) {
   return (
     <main className='main-container'>
       <div className='title-box'>
         <h2>Stays in Finland</h2>
         <p>12+ stays</p>
       </div>
-      <Stay />
+      <Stay 
+        stays={stays}
+        setStays={setStays}
+      />
     </main>
   )
 }
--- src/components/Stay.js
+++ src/components/Stay.js
@@ -1,24 +1,24 @@
 import '../stylesheets/Stay.css'
 import star from '../images/star.png'
 
-function Stay() {
+function Stay({ stays, setStays }) {
   return (
     <article className='stay-container'>
       <img
         className='stay-photo'
-        src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
+        src={stays[2].photo}
       />
       <div className='stay-subtitle-box'>
         <div className='stay-subtitle-box-left'>
           <h6 className='super-host-label'>SUPER HOST</h6>
-          <p className='stay-type'>Entire aparment. 2 beds.</p>
+          <p className='stay-type'>{stays[2].type}</p>
         </div>
         <div className='stay-subtitle-box-right'>
           <img src={star} className='star-icon' />
-          <p className='stay-rating'>4.40</p>
+          <p className='stay-rating'>{stays[2].rating}</p>
         </div>
       </div>
-      <h4 className='stay-title'>Lovely Studio near Railway Station</h4>
+      <h4 className='stay-title'>{stays[2].title}</h4>
     </article>
   )
 }
